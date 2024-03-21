cf7445b07c0f6eafa65b2607cf0312503003b267
Liran Cohen <c.liran.c@gmail.com>
2024-03-21 09:57:59 -0400

    removes vite logo

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
